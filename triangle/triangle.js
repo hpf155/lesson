bacfced7cc4ec7d8b9009e8383e9c63438cfa570
hpf155 <a464422152@vip.qq.com>
2015-04-28 12:54:57 +0800

    Update triangle.js

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -4,7 +4,12 @@ var triangle = {
     	    if(b!=c) return false ;
     		return true ;
     },
-    isIsosceles : function(a,b,c) {
-    	    return true ;
-    }
+     isIsosceles : function(a,b,c) {
+    	if(a==b||a==c||b==c) return true;
+    	    return false ;
+    },
+    isScalene : function(a,b,c) {
+    	if(a==b&&a==c) return true;
+	    return false ;
+}
 };
